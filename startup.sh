40b611375f644d78834c61d9e7a8ad2976a181c5
Hsieh <hunhsieh@a8667f01ae5b.ant.amazon.com>
2017-04-06 11:40:42 +0800

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,4 +8,5 @@ if [[ ! -z ${EXTRA_NGINX_CONF_URL} ]]; then
   wget ${EXTRA_NGINX_CONF_URL} -O /opt/openresty/nginx/conf/extra-locations.d/extra.conf
 fi
 
+[ -e /usr/local/bin/supervisord ] && ln -sf /usr/local/bin/supervisord /usr/bin/supervisord
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf --nodaemon
